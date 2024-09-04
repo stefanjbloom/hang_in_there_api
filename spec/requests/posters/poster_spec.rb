637b50677e20a113fab57b2c0daef04c3db8a556
Shane Galvin <Sgalvin36@gmail.com>
2024-09-04 15:07:26 -0600

    API_index

--- spec/requests/posters/poster_spec.rb
+++ spec/requests/posters/poster_spec.rb
@@ -1,1 +1,33 @@
 require 'rails_helper'
+
+describe "Posters API" do
+    it "sends a list of posters" do
+        Poster.create(name: "REGRET",
+            description: "Hard work rarely pays off.",
+            price: 89.00,
+            year: 2018,
+            vintage: true,
+            img_url:  "https://plus.unsplash.com/premium_photo-1661293818249-fddbddf07a5d")
+
+        Poster.create(name: "FAILURE",
+            description: "Why bother trying? It's probably not worth it.",
+            price: 68.00,
+            year: 2019,
+            vintage: true,
+            img_url: "https://images.unsplash.com/photo-1620401537439-98e94c004b0d")
+
+        Poster.create(name: "MEDIOCRITY",
+            description: "Dreams are just that—dreams.",
+            price: 127.00,
+            year: 2021,
+            vintage: false,
+            img_url: "https://images.unsplash.com/photo-1551993005-75c4131b6bd8")
+        
+        
+        get '/api/v1/posters'
+
+        expect(response).to be_successful
+
+        songs = JSON.parse(response.body, symbolize_names: true)
+    end
+end
