5a01897c4d293fd04cffa4f4df9b12c91455a82e
Stefan Bloom <stefanjbloom88@gmail.com>
2024-09-04 16:44:37 -0600

    Organization and merging branch to main

--- app/controllers/api/v1/posters_controller.rb
+++ app/controllers/api/v1/posters_controller.rb
@@ -2,22 +2,23 @@ class Api::V1::PostersController < ApplicationController
     def index
         render json: Poster.all
     end
-
+    
+    def show
+        render json: Poster.find(params[:id])
+    end
+    
     def create
         render json: Poster.create(poster_params)
     end
 
-    def show
-        render json: Poster.find(params[:id])
+    def update
+        render json: Poster.update(params[:id], poster_params)
     end
 
     def destroy
         render json: Poster.delete(params[:id])
     end
 
-    def update
-        render json: Poster.update(params[:id], poster_params)
-    end
     private
 
     def poster_params
--- config/routes.rb
+++ config/routes.rb
@@ -8,8 +8,8 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
   get "/api/v1/posters", to: "api/v1/posters#index"
-  post "/api/v1/posters", to: "api/v1/posters#create"
   get "/api/v1/posters/:id", to: "api/v1/posters#show"
-  delete "/api/v1/posters/:id", to: "api/v1/posters#destroy"
+  post "/api/v1/posters", to: "api/v1/posters#create"
   patch "/api/v1/posters/:id", to: "api/v1/posters#update"
+  delete "/api/v1/posters/:id", to: "api/v1/posters#destroy"
 end
