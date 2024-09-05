024818fb2854f8181f8c34d0a9cf75c24f6bb06c
Shane Galvin <Sgalvin36@gmail.com>
2024-09-05 15:52:31 -0600

    Add tests for filter by max_price

--- spec/requests/posters/poster_spec.rb
+++ spec/requests/posters/poster_spec.rb
@@ -174,4 +174,43 @@ describe "Posters API" do
             expect(poster[:attributes][:img_url]).to be_a(String)
         end
     end
+
+    it "can filter Posters by max_price" do
+        get '/api/v1/posters?max_price=88.00'
+
+        expect(response).to be_successful
+
+        posters = JSON.parse(response.body, symbolize_names: true)
+
+        expect(posters[:data].count).to eq(1)
+
+        posters[:data].each do |poster|
+            expect(poster).to have_key(:id)
+            expect(poster[:id].to_i).to be_an(Integer)
+
+            expect(poster).to have_key(:attributes)
+            expect(poster[:attributes]).to be_a(Hash)
+
+            expect(poster[:attributes]).to have_key(:name)
+            expect(poster[:attributes][:name]).to be_a(String)
+
+            expect(poster[:attributes]).to have_key(:description)
+            expect(poster[:attributes][:description]).to be_a(String)
+
+            expect(poster[:attributes]).to have_key(:price)
+            expect(poster[:attributes][:price]).to be_a(Float)
+            expect(poster[:attributes][:price]).to be <= 88.00
+
+            expect(poster[:attributes]).to have_key(:year)
+            expect(poster[:attributes][:year]).to be_a(Integer)
+
+            expect(poster[:attributes]).to have_key(:vintage)
+            expect([true, false]).to include(poster[:attributes][:vintage])
+
+            expect(poster[:attributes]).to have_key(:img_url)
+            expect(poster[:attributes][:img_url]).to be_a(String)
+        end
+    end
+
+    
 end
