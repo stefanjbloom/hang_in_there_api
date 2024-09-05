7ed0cae31ce09897d63aad07f0d949bacf4bd1d3
Shane Galvin <Sgalvin36@gmail.com>
2024-09-05 15:21:15 -0600

    Add scope functionality to model

--- app/models/poster.rb
+++ app/models/poster.rb
@@ -1,5 +1,5 @@
 class Poster < ApplicationRecord
-    scope :filter_by_name, ->(name = "") { where("name ILIKE ?", "%#{name}%") }
-    scope :filter_by_max, ->(price = 0) { where("price >= ?", price) }
-    scope :filter_by_min, ->(price = 0) { where("price <= ?", price) }
+    scope :filter_by_name, ->(name = nil) { name.present? ? where("name ILIKE ?", "%#{name}%") : all }
+    scope :filter_by_max, ->(price = nil) { price.present? ? where("price <= ?", price) : all}
+    scope :filter_by_min, ->(price = nil) { price.present? ? where("price >= ?", price) : all }
 end
