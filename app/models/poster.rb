b2bc3fd8dcfeafe6ea1d7d37d28c9d4f43f6ca3c
Shane Galvin <Sgalvin36@gmail.com>
2024-09-06 14:17:46 -0600

    create test for initial validation

--- app/models/poster.rb
+++ app/models/poster.rb
@@ -1,4 +1,10 @@
 class Poster < ApplicationRecord
+    validates :name, :description, :price, :year, :img_url, presence: {message: "is required"}
+    validates :price, numericality: {message: "should be a valid price"} 
+    validates :year, numericality: {only_integer: true, message: "should be a valid year"}
+    validates :vintage, inclusion: { in:[true, false], message: "should be true or false" }
+    validates :name, uniqueness: { case_sensitive: false }
+    
     scope :filter_by_name, ->(name = nil) { name.present? ? where("name ILIKE ?", "%#{name}%") : all }
     scope :filter_by_max, ->(price = nil) { price.present? ? where("price <= ?", price) : all}
     scope :filter_by_min, ->(price = nil) { price.present? ? where("price >= ?", price) : all }
